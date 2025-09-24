7e2b9dad7377ece98f5e057f6558165bf14d71ea
PlasThomas <joseplascencia346@aragon.unam.mx>
2025-09-23 19:31:20 -0600

    requisitos de python

---
+++ install.sh
@@ -0,0 +1,5 @@
+python3 -m venv prai
+source prai/bin/activate
+echo "==============> ENTORNO VIRTUAL CREADO <=============="
+pip install -r requirements.txt
+echo "==============> DEPENDENCIAS DE PYTHON INSTALADAS <=============="
