1e39e48c66b35034172d13996fad77e24928a05d
AngelGyr <angel18vega24@gmail.com>
2025-09-30 00:14:56 -0600

    agregado el csv limpio

--- db/scripts/cargar_csv.py
+++ db/scripts/cargar_csv.py
@@ -7,7 +7,7 @@ import os
 # Carga las variables del archivo .env
 load_dotenv('/home/thomas/Documentos/Escuela/Noveno_semestre/IA/ProyectoIA1/.env')
 
-csv_path = "/home/thomas/Documentos/Escuela/Noveno_semestre/IA/ProyectoIA1/db/data/data-2025-09-28.csv" #aqui es importante que modifiquen la ruta a donde este el csv
+csv_path = "/home/thomas/Documentos/Escuela/Noveno_semestre/IA/ProyectoIA1/db/data/data_geo_limpia.csv" #aqui es importante que modifiquen la ruta a donde este el csv
 
 #Creacion del motor de conexion con SQLAlchemy
 #usamos Mysql con el driver PyMySql
@@ -38,8 +38,11 @@ df_final = df[[
     "fuente"            # Fuente de los datos
 ]]
 
+# Renombrar 'alcaldi' a 'alcaldia' para que coincida exactamente con tu tabla
+df_final = df_final.rename(columns={'alcaldi': 'alcaldia'})
+
  #Se cargan los datos da la BD
-df_final.to_sql("atlas_inundaciones", con=engine, if_exists="replace", index=False)
+df_final.to_sql("atlas_inundaciones", con=engine, if_exists="append", index=False)
 
 #esto confirma si se realizo la ejecucion del programa
 print("Datos cargados en MySQL con éxito")
